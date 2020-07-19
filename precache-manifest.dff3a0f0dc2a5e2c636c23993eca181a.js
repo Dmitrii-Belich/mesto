5fc148d3fc278c43bca1f5fa75a67f22c4fbfd69
Дмитрий Белич <61316958+Dmitrii-Belich@users.noreply.github.com>
2020-07-19 13:26:22 +0300

    Update precache-manifest.dff3a0f0dc2a5e2c636c23993eca181a.js

--- precache-manifest.dff3a0f0dc2a5e2c636c23993eca181a.js
+++ precache-manifest.dff3a0f0dc2a5e2c636c23993eca181a.js
@@ -1,54 +1,54 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "dda8b096bf2465d6ecc5262f8ebc6a19",
-    "url": "/index.html"
+    "url": "./index.html"
   },
   {
     "revision": "30e915499da2d87cfcfd",
-    "url": "/static/css/main.a3d7d420.chunk.css"
+    "url": "./static/css/main.a3d7d420.chunk.css"
   },
   {
     "revision": "11b27456e704c7dd9f53",
-    "url": "/static/js/2.9b7c019d.chunk.js"
+    "url": "./static/js/2.9b7c019d.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/static/js/2.9b7c019d.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.9b7c019d.chunk.js.LICENSE.txt"
   },
   {
     "revision": "30e915499da2d87cfcfd",
-    "url": "/static/js/main.2f005c54.chunk.js"
+    "url": "./static/js/main.2f005c54.chunk.js"
   },
   {
     "revision": "9faa06e49578191d5a13",
-    "url": "/static/js/runtime-main.66dd7950.js"
+    "url": "./static/js/runtime-main.66dd7950.js"
   },
   {
     "revision": "09f4068bb74fc78ffbce74f5596d6aa5",
-    "url": "/static/media/Inter-Black.09f4068b.woff2"
+    "url": "./static/media/Inter-Black.09f4068b.woff2"
   },
   {
     "revision": "e37354838bf8078ac0d296217613533f",
-    "url": "/static/media/Inter-Black.e3735483.woff"
+    "url": "./static/media/Inter-Black.e3735483.woff"
   },
   {
     "revision": "7a8cc7241f766a142e15b2948804e547",
-    "url": "/static/media/Inter-Medium.7a8cc724.woff"
+    "url": "./static/media/Inter-Medium.7a8cc724.woff"
   },
   {
     "revision": "f6cf0a0bc5fce3307e2c426eb14eb752",
-    "url": "/static/media/Inter-Medium.f6cf0a0b.woff2"
+    "url": "./static/media/Inter-Medium.f6cf0a0b.woff2"
   },
   {
     "revision": "4dd66a113d54a7f9a1ae913049610617",
-    "url": "/static/media/Inter-Regular.4dd66a11.woff2"
+    "url": "./static/media/Inter-Regular.4dd66a11.woff2"
   },
   {
     "revision": "7c539936c4c8c822b59a1bcc6c08a6ec",
-    "url": "/static/media/Inter-Regular.7c539936.woff"
+    "url": "./static/media/Inter-Regular.7c539936.woff"
   },
   {
     "revision": "e889e45f7898567d7ada789a8ffa0e63",
-    "url": "/static/media/logo.e889e45f.svg"
+    "url": "./static/media/logo.e889e45f.svg"
   }
 ]);
