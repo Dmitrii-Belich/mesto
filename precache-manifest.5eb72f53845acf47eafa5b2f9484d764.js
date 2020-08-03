c04393b365f4bc69ab5350e1bd4f3c22123a5137
Dmitrii Belich <dim1999@mail.ru>
2020-08-03 16:33:10 +0300

    Updates

---
+++ precache-manifest.5eb72f53845acf47eafa5b2f9484d764.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2e23e7ba3e5c022999ad10b4dcb6251",
+    "url": "/mesto/index.html"
+  },
+  {
+    "revision": "eebce870932d312e6156",
+    "url": "/mesto/static/css/main.5fa044e8.chunk.css"
+  },
+  {
+    "revision": "eafb3c1c08da567ddf19",
+    "url": "/mesto/static/js/2.b0f9ddad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mesto/static/js/2.b0f9ddad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eebce870932d312e6156",
+    "url": "/mesto/static/js/main.1e264f69.chunk.js"
+  },
+  {
+    "revision": "738a12e212008e0b74fd",
+    "url": "/mesto/static/js/runtime-main.db004d61.js"
+  },
+  {
+    "revision": "09f4068bb74fc78ffbce74f5596d6aa5",
+    "url": "/mesto/static/media/Inter-Black.09f4068b.woff2"
+  },
+  {
+    "revision": "e37354838bf8078ac0d296217613533f",
+    "url": "/mesto/static/media/Inter-Black.e3735483.woff"
+  },
+  {
+    "revision": "7a8cc7241f766a142e15b2948804e547",
+    "url": "/mesto/static/media/Inter-Medium.7a8cc724.woff"
+  },
+  {
+    "revision": "f6cf0a0bc5fce3307e2c426eb14eb752",
+    "url": "/mesto/static/media/Inter-Medium.f6cf0a0b.woff2"
+  },
+  {
+    "revision": "4dd66a113d54a7f9a1ae913049610617",
+    "url": "/mesto/static/media/Inter-Regular.4dd66a11.woff2"
+  },
+  {
+    "revision": "7c539936c4c8c822b59a1bcc6c08a6ec",
+    "url": "/mesto/static/media/Inter-Regular.7c539936.woff"
+  },
+  {
+    "revision": "e889e45f7898567d7ada789a8ffa0e63",
+    "url": "/mesto/static/media/logo.e889e45f.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.dff3a0f0dc2a5e2c636c23993eca181a.js"
+  "/mesto/precache-manifest.5eb72f53845acf47eafa5b2f9484d764.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mesto/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
